784b9a9b4da5add68f0a8a6b2ef932f47f4d1a23
mazidul36i <islammazidul1369@gmail.com>
2022-12-13 22:22:07 +0530

    Initial commit

---
+++ src/main/java/com/masai/app/MarrsBankApplication.java
@@ -0,0 +1,13 @@
+package com.masai.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarrsBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarrsBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masai/app/MarrsBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masai.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarrsBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
