645a4dcedf54c87e1c9734f3290ddf845e006355
avanishmani <AvanishmaniTripathi@gmail.com>
2022-12-15 13:49:59 +0530

    CustomerException.java

---
+++ src/main/java/com/masai/app/exceptions/CustomerException.java
@@ -0,0 +1,11 @@
+package com.masai.app.exceptions;
+
+public class CustomerException extends Exception {
+public CustomerException() {
+	// TODO Auto-generated constructor stub
+}
+public CustomerException(String msg) {
+	// TODO Auto-generated constructor stub
+	super(msg);
+}
+}
