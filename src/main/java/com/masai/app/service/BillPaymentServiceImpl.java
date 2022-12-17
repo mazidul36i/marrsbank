3403c8447737b2fc02bf85fa359f0d1a4544dae7
Rajni Kant Arya <101392769+rkmasai@users.noreply.github.com>
2022-12-17 23:57:41 +0530

    update

--- src/main/java/com/masai/app/service/BillPaymentServiceImpl.java
+++ src/main/java/com/masai/app/service/BillPaymentServiceImpl.java
@@ -27,38 +27,46 @@ public class BillPaymentServiceImpl implements BillPaymentService {
 	@Override
 	public BillPayment addBillPayment(BillPayment payment, Integer WalletId)
 			throws WalletException, BillPaymentException {
-
+			
+		
 		Wallet wallet = walletDao.findById(WalletId)
 				.orElseThrow(() -> new WalletException("Wallet doesn't not found!"));
+	
+		long wall = (long) (wallet.getBalance() - payment.getAmount());
+			
+			if(wall > 0) {
+				wallet.setBalance(wall);
+				payment.setWallet(wallet);
 
-		payment.setWallet(wallet);
-
-		return billDao.save(payment);
+				return billDao.save(payment);
+			}
+			else {
+			throw  new WalletException("Unsufficient balance Please Wallet update");
+			}	
 	}
 
 	@Override
 	public BillPayment viewBillPayment(Integer billId) throws BillPaymentException {
 
-//	return billDao.findByBillId(billId).orElseThrow(() ->
-//	new BillPaymentException("Bill Details not found"))	;
-
-		BillPayment bill = billDao.findByBillId(billId);
+			BillPayment bill = billDao.findByBillId(billId);
 
 		if (bill != null) {
 
 			return bill;
 		} else
 			throw new BillPaymentException("Bill Details not Found");
-
 	}
-
+	
 	@Override
 	public List<BillPayment> viewAllBillPayments(Integer walletId) throws BillPaymentException, WalletException {
+		
 		Wallet wallet = walletDao.findById(walletId)
 				.orElseThrow(() -> new WalletException("Wallet doesn't exists with the wallet Id: " + walletId));
+		
 		if (wallet.getBillPayments() == null || wallet.getBillPayments().size() == 0)
 			throw new BillPaymentException("No bill made on this wallet!");
-		return wallet.getBillPayments();
+		
+			return wallet.getBillPayments();
 	}
 
 }
